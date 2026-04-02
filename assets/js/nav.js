e9bd7701bbbf37a863d93aa95f4feec0e584594e
Tim Wilding <tim_wilding@yahoo.com>
2026-04-02 11:50:07 +0100

    fix to wellcome logo and sdsct logo

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -12,9 +12,10 @@ document.addEventListener("DOMContentLoaded", () => {
           <ul>
               <li><a href="index.html">Home</a></li>
               <li><a href="vision.html">Our Vision</a></li>
-              <li><a href="research.html">What's Happening</a></li>
+              <li><a href="blogs.html">What's Happening</a></li>
+              <li><a href="research.html">Research</a></li>
+              <li><a href="events.html">Events</a></li>
               <li><a href="team.html">Meet the Team</a></li>
-              <li><a href="resources.html">Resources</a></li>
               <li><a href="contact.html">Contact Us</a></li>
           </ul>
       </nav>
@@ -26,14 +27,14 @@ document.addEventListener("DOMContentLoaded", () => {
   const footHTML= `
     <ul class="copyright">
       <li class="left">Centre for Medical Informatics<br>Usher Institute<br>5-7 Little France Road<br>Edinburgh BioQuarter - Gate 3<br>Edinburgh EH16 4UX</p><p><a href="privacy.html">Privacy Notice</a></p></li>
-      <li class="right-group" style="text-align: right;">
-        <img src="images/SDSCT-logo.png" alt="Wellcome" class="logo">
-        T<br>
-      </li>
-      <li class="right-group" style="text-align: right;">
-        <span class="funded">Funded by</span>
-        <img src="images/wellcome-logo.jpg" alt="Wellcome" class="logo">
-        [308485/Z/23/Z]<br>
+       <li class="right-group" style="text-align: right;">
+        <img src="images/SDSCT-logo.png" alt="SDSCT" class="logo sdsct-logo">
+        <div class="wellcome-stack">
+          <span class="funded">Funded by</span>
+          <img src="images/wellcome-logo.jpg" alt="Wellcome" class="logo">
+          <span class="grant-number">[308485/Z/23/Z]</span>
+        </div>
+
       </li>
     </ul>
   `;
